1a7f70a5d70c212908bc8cdfefbb46e0b434d832
misatoalexandre <misatotina.alexandre@gmail.com>
2013-10-02 14:59:12 -0400

    Initial Commit

---
+++ Camera RollTests/Camera_RollTests.m
@@ -0,0 +1,34 @@
+//
+//  Camera_RollTests.m
+//  Camera RollTests
+//
+//  Created by Misato Tina Alexandre on 10/2/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Camera_RollTests : XCTestCase
+
+@end
+
+@implementation Camera_RollTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
